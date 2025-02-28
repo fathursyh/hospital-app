2265427670a02185a01027e902e82ccc1561ec74
fathursyh <fathur.syah1@gmail.com>
2025-02-28 09:18:59 +0700

    first commit

---
+++ resources/views/tes.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>live</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+    <h1>tes</h1>
+    <livewire:counter />
+</body>
+</html>
