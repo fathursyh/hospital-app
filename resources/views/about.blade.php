cbe110cbabeeb0b6d7e91099a703790dffc94796
Fathursyah Awandanu <fathur.syah1@gmail.com>
2025-03-01 21:47:32 +0700

    update : service view, about view

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -9,10 +9,10 @@
                     <img src="https://plus.unsplash.com/premium_photo-1681842978092-3af31637042a?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="doctor and pasien image from unsplash" referrerpolicy="no-referrer" class="w-full h-44 md:h-80 object-cover overflow-hidden mb-4 rounded" style="object-position: 0px -4rem">
                     <h2 class="text-3xl font-semibold">About Us</h2>
                     <p class="mt-4">
-                        At [Hospital Name], we are committed to providing exceptional healthcare services to our community. Our team of dedicated professionals works tirelessly to ensure that every patient receives the highest quality of care in a compassionate and respectful environment.
+                        At F-Hospital, we are committed to providing exceptional healthcare services to our community. Our team of dedicated professionals works tirelessly to ensure that every patient receives the highest quality of care in a compassionate and respectful environment.
                     </p>
                     <p class="mt-4">
-                        Established in [Year], our hospital has grown to become a leading healthcare provider in the region. We offer a wide range of medical services, including emergency care, surgery, maternity, and specialized treatments, all designed to meet the diverse needs of our patients.
+                        Established in 2020, our hospital has grown to become a leading healthcare provider in the region. We offer a wide range of medical services, including emergency care, surgery, maternity, and specialized treatments, all designed to meet the diverse needs of our patients.
                     </p>
                     <p class="mt-4">
                         Our state-of-the-art facilities are equipped with the latest technology, and our staff is continuously trained in the best practices to ensure that we remain at the forefront of medical advancements.
--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -3,21 +3,23 @@
         Services
     @endsection
     @section('main')
-        <section class="w-screen flex h-[90.4vh] mt-18">
-            
-
-<div class="grid grid-cols-2 gap-2 w-full">
-    <div class="rounded-lg bg-gray-200 dark:bg-zinc-900">
-    </div>
-    <div class="rounded-lg bg-gray-200 dark:bg-zinc-900">
-    </div>
-    <div class="rounded-lg bg-gray-200 dark:bg-zinc-900">
-    </div>
-    <div class="rounded-lg bg-gray-200 dark:bg-zinc-900">
-    </div>
-</div>
-
-
+        <section class="w-screen h-[90.4vh] max-h-fit mt-18 grid md:grid-cols-2 grid-cols-1 md:grid-rows-2 grid-rows-4 gap-2 rounded-lg">
+            <div class="rounded-lg bg-green-300 dark:bg-green-400 overflow-hidden relative grid place-items-center cursor-pointer">
+                <img src="https://images.unsplash.com/photo-1551076805-e1869033e561?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="operation" class="opacity-65 hover:opacity-90">
+                <p class="absolute text-blue-500 font-bold md:text-6xl text-5xl drop-shadow-md rounded-lg pointer-events-none">Surgery</p>
+            </div>
+            <div class="rounded-lg bg-blue-300 dark:bg-blue-400 overflow-hidden relative grid place-items-center cursor-pointer">
+                <img src="https://images.unsplash.com/photo-1554734867-bf3c00a49371?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="emergency" class="opacity-65 hover:opacity-90">
+                <p class="absolute text-white font-bold md:text-6xl text-5xl drop-shadow-md rounded-lg pointer-events-none">Emergency</p>
+            </div>
+            <div class="rounded-lg bg-amber-300 dark:bg-amber-400 overflow-hidden relative grid place-items-center cursor-pointer">
+                <img src="https://images.unsplash.com/photo-1574043948184-144f2ef80fe3?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="medicine" class="opacity-65 hover:opacity-90">
+                <p class="absolute text-blue-500 font-bold md:text-6xl text-5xl drop-shadow-md rounded-lg pointer-events-none">Medicine</p>
+            </div>
+            <div class="rounded-lg bg-red-300 dark:bg-red-400 overflow-hidden relative grid place-items-center cursor-pointer">
+                <img src="https://plus.unsplash.com/premium_photo-1673953510197-0950d951c6d9?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="consultation" class="opacity-65 hover:opacity-90">
+                <p class="absolute text-blue-500 font-bold md:text-6xl text-5xl drop-shadow-md rounded-lg pointer-events-none">Consultation</p>
+            </div>
         </section>
     @endsection
 </x-layouts.app>
