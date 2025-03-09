7740406fabdf8da41f98c96950a66c8e412f9a16
fathursyh <fathur.syah1@gmail.com>
2025-03-09 23:03:53 +0700

    update : small style adjustment

--- resources/views/dashboard-appointments.blade.php
+++ resources/views/dashboard-appointments.blade.php
@@ -1,5 +1,5 @@
 <x-layouts.dashboard.admin>
-    <div class="p-4 sm:ml-64 md:h-screen h-[90vh]">
+    <div class="p-4 sm:ml-64 md:h-screen min-h-[90vh]">
         <div class="border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 h-full box-border">
                <livewire:appointment-list />
         </div>
--- resources/views/livewire/appointment-list.blade.php
+++ resources/views/livewire/appointment-list.blade.php
@@ -1,5 +1,7 @@
-<div class="w-full h-fit p-4 flex gap-4 flex-wrap ">
-    @foreach ($list as $patient)
-        <x-appointment-card :patient="$patient" />
-    @endforeach
+<div class="mt-10 md:mt-0 pb-10">
+    <div class="w-[99%] xl:w-full 2xl:w-[99%] mx-auto h-fit flex gap-3 flex-wrap ">
+        @foreach ($list as $patient)
+            <x-appointment-card :patient="$patient" />
+        @endforeach
+    </div>
 </div>
