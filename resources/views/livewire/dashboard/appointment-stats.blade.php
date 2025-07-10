8fadf29176b0e4f685f635be1790cdbfbcfd8c88
Fathursyah Awandanu <fathur.syah04@gmail.com>
2025-07-10 15:13:10 +0700

    minor update: change hover bg color on dashboard patient stats

--- resources/views/livewire/dashboard/appointment-stats.blade.php
+++ resources/views/livewire/dashboard/appointment-stats.blade.php
@@ -7,7 +7,7 @@
         <div class="w-full h-full flex flex-col justify-start 2xl:justify-evenly">
             @foreach ($latest as $item)
             <a href="/">
-                <div class="grid grid-cols-2 divide-x-2 {{ date_create($item->patient->date) < date_create() ? 'bg-red-300 dark:bg-red-800/60' : 'bg-white dark:bg-zinc-800' }} border rounded hover:bg-blue-100 dark:hover:bg-blue-900">
+                <div class="grid grid-cols-2 divide-x-2 {{ date_create($item->patient->date) < date_create() ? 'bg-red-300 dark:bg-red-800/60' : 'bg-white dark:bg-zinc-800' }} border rounded hover:bg-red-950/60">
                     <p class="py-2 px-4 flex items-center">{{ $item->patient->fullname }}</p>
                     <p class="py-2 px-4 flex items-center">{{ date_create($item->patient->date)->format('l, d F') }}</p>
                 </div>
